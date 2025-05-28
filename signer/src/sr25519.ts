d0238d46d8d00ca175f10b9802dc43dda071d52f
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2025-05-28 01:49:55 -0500

    fix(@virtonetwork/signer): use hdkd utils to generate a valid sr25519 key pair

--- signer/src/sr25519.ts
+++ signer/src/sr25519.ts
@@ -1,15 +1,18 @@
 import { PolkadotSigner, SS58String } from "polkadot-api";
-import { sr25519, ss58Encode } from "@polkadot-labs/hdkd-helpers";
+import {
+  entropyToMiniSecret,
+  generateMnemonic,
+  mnemonicToEntropy,
+  ss58Encode,
+} from "@polkadot-labs/hdkd-helpers";
 
 import { getPolkadotSigner } from "polkadot-api/signer";
-import { randomBytes } from "@noble/hashes/utils";
+import { sr25519CreateDerive } from "@polkadot-labs/hdkd";
 
 /** A function that signs */
 export type SignFn = (input: Uint8Array) => Promise<Uint8Array> | Uint8Array;
 
 export type Sr25519Signer = {
-  /** keep it safe – this is your raw 32-byte secret */
-  secret: Uint8Array;
   /** PAPI-compatible signer object, with the `sign` function exposed (for testing purposes) */
   signer: ReturnType<typeof getPolkadotSigner> & { sign: SignFn };
   /** MultiAddress-wrapped SS58 address (prefix 0 = Polkadot) */
@@ -21,16 +24,21 @@ export type Sr25519Signer = {
  * @param prefix SS58 network prefix – 0 = Polkadot, 2 = Kusama, 42 = generic …
  */
 export function createSessionKeySigner(prefix: number = 0): Sr25519Signer {
-  const secret = randomBytes(32);
+  const miniSecret = entropyToMiniSecret(
+    mnemonicToEntropy(generateMnemonic(256))
+  );
+  const derive = sr25519CreateDerive(miniSecret);
 
-  const publicKey = sr25519.getPublicKey(secret);
-  const sign = (input: Uint8Array) => sr25519.sign(input, secret);
-  const signer = getPolkadotSigner(publicKey, "Sr25519", sign);
-  Object.defineProperty(signer, "sign", { value: sign, configurable: false });
+  const keypair = derive(`//${Date.now()}`);
+
+  const signer = getPolkadotSigner(keypair.publicKey, "Sr25519", keypair.sign);
+  Object.defineProperty(signer, "sign", {
+    value: keypair.sign,
+    configurable: false,
+  });
 
   return {
-    secret,
     signer: signer as PolkadotSigner & { sign: SignFn },
-    address: ss58Encode(publicKey, prefix),
+    address: ss58Encode(keypair.publicKey, prefix),
   };
 }
